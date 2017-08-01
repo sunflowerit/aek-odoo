9771c3da242d44d879164dc75ed166aaff750a23
terrence <nza.terrence@gmail.com>
2017-08-01 15:17:33 +0300

    Stop autorefresh on form view

--- aek_autorefresh/static/src/js/autorefresh.js
+++ aek_autorefresh/static/src/js/autorefresh.js
@@ -21,25 +21,25 @@ openerp.aek_autorefresh = function (instance) {
 	    }
     });
 	
-	instance.web.FormView.include({
-		init: function(parent, dataset, view_id, options) {
-			var self = this;
-			this._super.apply(this, arguments);
-			if(parent.action && parent.action.auto_refresh > 0){
-				self.refresh_interval = setInterval(_.bind(function(){ 
-					if(this.$el[0].clientWidth != 0 && this.dataset.index != null){
-						this.reload();
-					}
-				}, self), parent.action.auto_refresh*1000);
-			}
-	    },
-	    destroy : function() {
-	    	this._super.apply(this, arguments);
-	    	if(this.refresh_interval){
-	    		clearInterval(this.refresh_interval);
-	    	}
-	    }
-    });
+    // instance.web.FormView.include({
+		// init: function(parent, dataset, view_id, options) {
+		// 	var self = this;
+		// 	this._super.apply(this, arguments);
+		// 	if(parent.action && parent.action.auto_refresh > 0){
+		// 		self.refresh_interval = setInterval(_.bind(function(){
+		// 			if(this.$el[0].clientWidth != 0 && this.dataset.index != null){
+		// 				this.reload();
+		// 			}
+		// 		}, self), parent.action.auto_refresh*1000);
+		// 	}
+	 //    },
+	 //    destroy : function() {
+	 //    	this._super.apply(this, arguments);
+	 //    	if(this.refresh_interval){
+	 //    		clearInterval(this.refresh_interval);
+	 //    	}
+	 //    }
+    // });
 	
 	instance.web_calendar.CalendarView.include({
 		init: function(parent, dataset, view_id, options) {
